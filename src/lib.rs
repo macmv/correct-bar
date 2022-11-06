4914cd604901833f40174c983698372f5c07621b
Neil Macneale V <neil.macneale.v@gmail.com>
2022-11-06 01:55:37 -0700

    Update config to render below the gnome bar

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@ pub use config::Config;
 pub fn run() {
   let config = Config {
     window: config::WindowConfig {
-      margin_top: 50,
-      margin_left: 50,
-      margin_right: 50,
-      margin_bottom: 50,
-      width: 1920 - 100,
+      margin_top: 40,
+      margin_left: 10,
+      margin_right: 10,
+      margin_bottom: 10,
+      width: 1920 - 20,
       height: 50,
       ..Default::default()
     },
