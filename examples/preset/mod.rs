013609a0498ebb953030b611ed8c5d032b6c4670
Neil Macneale V <neil.macneale.v@gmail.com>
2022-11-11 23:37:18 -0800

    Setup local and utc time with chrono

--- examples/preset/mod.rs
+++ examples/preset/mod.rs
@@ -1,6 +1,7 @@
 pub mod desktop;
 pub mod laptop;
 
+use chrono::{Datelike, Timelike};
 use correct_bar::bar::{Color, Module, ModuleImpl, Updater};
 use std::time::Duration;
 
@@ -9,9 +10,24 @@ struct TimeModule;
 impl ModuleImpl for TimeModule {
   fn updater(&self) -> Updater { Updater::Every(Duration::from_secs(1)) }
   fn render(&self, ctx: &mut correct_bar::bar::RenderContext) {
-    let now = std::time::SystemTime::now();
-    let dur = now.duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap();
-    ctx.draw_text(&format!("{}", dur.as_secs()), Color::white());
+    let local = chrono::Local::now();
+    let utc = local.naive_utc();
+    ctx.draw_text(
+      &format!(
+        "{}, {:04}-{:02}-{:02} at {:02}:{:02}:{:02} or {:02}:{:02}:{:02}",
+        local.weekday(),
+        local.year(),
+        local.month(),
+        local.day(),
+        local.hour(),
+        local.minute(),
+        local.second(),
+        utc.hour(),
+        utc.minute(),
+        utc.second(),
+      ),
+      Color::white(),
+    );
   }
 }
 
@@ -32,7 +48,6 @@ pub fn modules() -> (Vec<Module>, Vec<Module>, Vec<Module>) {
     vec![
       Module::text("mmm things", Color { r: 255, g: 100, b: 128 }).into(),
       sep().into(),
-      Module::text("time here", Color { r: 100, g: 255, b: 128 }).into(),
       Module::from(TimeModule).into(),
     ],
   )
