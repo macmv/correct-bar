97c0bcf69d4ef16b89a4b2113906608160bf34ea
Neil Macneale V <neil.macneale.v@gmail.com>
2022-11-29 16:31:02 -0800

    Scale x and y without swapping x and y

--- src/math.rs
+++ src/math.rs
@@ -56,7 +56,7 @@ impl Rect {
 
   pub fn scaled_by(mut self, scale: f64) -> Self {
     self.pos.x = (self.pos.x as f64 * scale) as i32;
-    self.pos.y = (self.pos.x as f64 * scale) as i32;
+    self.pos.y = (self.pos.y as f64 * scale) as i32;
     self.width = (self.width as f64 * scale) as u32;
     self.height = (self.height as f64 * scale) as u32;
     self
