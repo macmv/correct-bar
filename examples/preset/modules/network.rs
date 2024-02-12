844527d512a4409b48e0d921b3797b6ea7bf20ca
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-11 20:44:02 -0800

    Start calling dbus by hand

--- examples/preset/modules/network.rs
+++ examples/preset/modules/network.rs
@@ -1,12 +1,14 @@
 use correct_bar::bar::{Color, ModuleImpl, Updater};
 use crossbeam_channel::Receiver;
-use dbus::blocking::Connection;
+use dbus::blocking::{stdintf::org_freedesktop_dbus::Properties, Connection};
 use networkmanager::{
   devices::{Any, Wired},
   NetworkManager,
 };
 use parking_lot::Mutex;
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
+
+type Result<T> = std::result::Result<T, dbus::Error>;
 
 #[derive(Clone)]
 pub struct Network {
@@ -26,7 +28,7 @@ impl Network {
     Self::new_inner(primary, secondary).unwrap()
   }
 
-  fn new_inner(primary: Color, secondary: Color) -> Result<Network, ()> {
+  fn new_inner(primary: Color, secondary: Color) -> Result<Network> {
     let state = NetworkState::new();
 
     let (tx, rx) = crossbeam_channel::bounded(0);
@@ -40,7 +42,7 @@ impl ModuleImpl for Network {
   fn render(&self, ctx: &mut correct_bar::bar::RenderContext) {
     let s = self.state.lock();
 
-    for (i, c) in s.active_connection().iter().enumerate() {
+    for (i, c) in s.active_connections().unwrap().iter().enumerate() {
       if i != 0 {
         ctx.draw_text(", ", self.secondary);
       }
@@ -50,10 +52,35 @@ impl ModuleImpl for Network {
   fn box_clone(&self) -> Box<dyn ModuleImpl + Send + Sync> { Box::new(self.clone()) }
 }
 
+const TIMEOUT: Duration = Duration::from_secs(5);
+
 impl NetworkState {
   pub fn new() -> NetworkState { NetworkState { dbus: Connection::new_system().unwrap() } }
 
-  pub fn active_connection(&self) -> Vec<String> {
+  pub fn active_connections(&self) -> Result<Vec<String>> {
+    let proxy = self.dbus.with_proxy(
+      "org.freedesktop.NetworkManager",
+      "/org/freedesktop/NetworkManager",
+      TIMEOUT,
+    );
+
+    let devices = proxy
+      .method_call::<(Vec<dbus::Path>,), _, _, _>(
+        "org.freedesktop.NetworkManager",
+        "GetDevices",
+        (),
+      )?
+      .0;
+
+    for dev in devices {
+      let proxy = self.dbus.with_proxy("org.freedesktop.NetworkManager", &dev, TIMEOUT);
+
+      let active =
+        proxy.get::<dbus::Path>("org.freedesktop.NetworkManager.Device", "ActiveConnection")?;
+
+      dbg!(&active);
+    }
+
     let nm = NetworkManager::new(&self.dbus);
     let mut connections = vec![];
 
@@ -73,6 +100,6 @@ impl NetworkState {
       }
     }
 
-    connections
+    Ok(connections)
   }
 }
