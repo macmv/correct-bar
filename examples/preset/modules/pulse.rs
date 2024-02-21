21d561c3d3b9527ce29c49cb57230e77a657fc87
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-20 23:51:19 -0800

    Can fetch server info

--- examples/preset/modules/pulse.rs
+++ examples/preset/modules/pulse.rs
@@ -107,6 +107,25 @@ impl Context {
   pub fn get_state(&self) -> ContextState {
     unsafe { ContextState::from_sys(sys::pa_context_get_state(self.pa)) }
   }
+
+  pub fn get_server_info(&self, custom: impl FnOnce(&ServerInfo) + Send + 'static) {
+    static CALLBACK: Mutex<Option<Box<dyn FnOnce(&ServerInfo) + Send>>> = Mutex::new(None);
+    extern "C" fn cb(
+      _ctx: *mut sys::pa_context,
+      info: *const sys::pa_server_info,
+      _userdata: *mut c_void,
+    ) {
+      let mut cb = CALLBACK.lock();
+      if let Some(cb) = cb.take() {
+        cb(&ServerInfo { pa: info });
+      }
+    }
+
+    CALLBACK.lock().replace(Box::new(custom));
+    unsafe {
+      sys::pa_context_get_server_info(self.pa, Some(cb), ptr::null_mut());
+    }
+  }
 }
 
 struct PropList {
@@ -182,6 +201,63 @@ impl ContextState {
   }
 }
 
+struct ServerInfo {
+  pa: *const sys::pa_server_info,
+}
+
+impl ServerInfo {
+  /// User name of the daemon process.
+  fn user_name(&self) -> &str { unsafe { CStr::from_ptr((*self.pa).user_name).to_str().unwrap() } }
+
+  /// Host name the daemon is running on.
+  fn host_name(&self) -> &str { unsafe { CStr::from_ptr((*self.pa).host_name).to_str().unwrap() } }
+
+  /// Version string of the daemon.
+  fn server_version(&self) -> &str {
+    unsafe { CStr::from_ptr((*self.pa).server_version).to_str().unwrap() }
+  }
+
+  /// Server package name (usually "pulseaudio").
+  fn server_name(&self) -> &str {
+    unsafe { CStr::from_ptr((*self.pa).server_name).to_str().unwrap() }
+  }
+
+  /// Default sample specification
+  fn sample_spec(&self) -> sys::pa_sample_spec { unsafe { (*self.pa).sample_spec } }
+
+  /// Name of default sink.
+  fn default_sink_name(&self) -> &str {
+    unsafe { CStr::from_ptr((*self.pa).default_sink_name).to_str().unwrap() }
+  }
+
+  /// Name of default source.
+  fn default_source_name(&self) -> &str {
+    unsafe { CStr::from_ptr((*self.pa).default_source_name).to_str().unwrap() }
+  }
+
+  /// A random cookie for identifying this instance of PulseAudio.
+  fn cookie(&self) -> u32 { unsafe { (*self.pa).cookie } }
+
+  /// Default channel map.
+  fn channel_map(&self) -> sys::pa_channel_map { unsafe { (*self.pa).channel_map } }
+}
+
+impl fmt::Debug for ServerInfo {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    f.debug_struct("ServerInfo")
+      .field("user_name", &self.user_name())
+      .field("host_name", &self.host_name())
+      .field("server_version", &self.server_version())
+      .field("server_name", &self.server_name())
+      .field("sample_spec", &self.sample_spec())
+      .field("default_sink_name", &self.default_sink_name())
+      .field("default_source_name", &self.default_source_name())
+      .field("cookie", &self.cookie())
+      .field("channel_map", &self.channel_map())
+      .finish()
+  }
+}
+
 impl Pulse {
   pub fn new(color: Color) -> Self {
     let (tx, rx) = crossbeam_channel::bounded(16);
@@ -198,6 +274,10 @@ impl Pulse {
         move || {
           if ctx.get_state() == ContextState::Ready {
             println!("ready");
+
+            ctx.get_server_info(|info| {
+              println!("got server info: {:?}", info);
+            });
           }
         }
       });
