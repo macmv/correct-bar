ddd29ee88c056bcca2f7bd06cceb6451cb123195
Neil Macneale V <neil.macneale.v@gmail.com>
2025-12-12 19:47:41 -0800

    Add Render::draw

--- cb-core/src/lib.rs
+++ cb-core/src/lib.rs
@@ -1,11 +1,14 @@
 use parley::{FontContext, LayoutContext};
-use vello::Scene;
+use peniko::color::palette;
+use vello::{RenderParams, Scene};
 
 pub struct RenderStore {
   font:   FontContext,
   layout: LayoutContext,
 
-  render: vello::Renderer,
+  render:       vello::Renderer,
+  texture:      wgpu::Texture,
+  texture_view: wgpu::TextureView,
 }
 
 pub struct Render<'a> {
@@ -13,4 +16,23 @@ pub struct Render<'a> {
   scene: Scene,
 }
 
-impl Render<'_> {}
+impl Render<'_> {
+  pub fn draw(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) {
+    self
+      .store
+      .render
+      .render_to_texture(
+        device,
+        queue,
+        &self.scene,
+        &self.store.texture_view,
+        &RenderParams {
+          base_color:          palette::css::BLACK,
+          width:               self.store.texture.width(),
+          height:              self.store.texture.height(),
+          antialiasing_method: vello::AaConfig::Msaa16,
+        },
+      )
+      .unwrap();
+  }
+}
