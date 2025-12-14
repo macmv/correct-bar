0472f1cdec0ab30ceda8f3d7028f449c7487697e
Neil Macneale V <neil.macneale.v@gmail.com>
2025-12-13 20:02:20 -0800

    Add a clock on the right

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 fn main() {
   cb_bar::run(cb_bar::Config {
     bars: vec![cb_bar::Bar {
-      left_modules:   vec![
-        Box::new(cb_builtin::Clock {
-          primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-          secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-        }),
-        Box::new(cb_builtin::Clock {
-          primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-          secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-        }),
-      ],
+      left_modules:   vec![Box::new(cb_builtin::Clock {
+        primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
+        secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
+      })],
       center_modules: vec![],
-      right_modules:  vec![],
+      right_modules:  vec![Box::new(cb_builtin::Clock {
+        primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
+        secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
+      })],
     }],
   });
 }
