45211cddd8f6a5254efa405f439e7142ff4b59f1
Neil Macneale V <neil.macneale.v@gmail.com>
2025-12-14 16:04:02 -0800

    Make the defaults look a little better

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
   cb_bar::run(cb_bar::Config {
     make_bar: || cb_bar::Bar {
-      left_modules:   vec![cb_builtin::Clock {
-        primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-        secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-      }
-      .into()],
+      left_modules:   vec![],
       center_modules: vec![],
       right_modules:  vec![cb_builtin::Clock {
-        primary:   peniko::Color::from_rgb8(0x46, 0x46, 0x46),
-        secondary: peniko::Color::from_rgb8(0x46, 0x46, 0x46),
+        primary:   peniko::Color::WHITE,
+        secondary: peniko::Color::from_rgb8(0x80, 0x80, 0x80),
       }
       .into()],
     },
